4203b0bbb1680d51d3c9918d817f8b934519a151
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-20 20:12:04 -0500

    titile and paragraph

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div data-component-id="comp" data-component-type="aesop">
+        <section>
+          <div className="wrapper">
+            <div>
+              <h1 className="title">Clensing, nourishing hand care </h1>
+              <div>
+                <p>
+                  Washing hands with vigour and rigour is an essential act. It
+                  is best followed by restorative, aromatic hydration: just like
+                  the uneasy mind, hard-working hands deserve attentive
+                  nurturing.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
