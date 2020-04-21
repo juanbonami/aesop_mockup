ffe41f3e337a2c9b32fc2c4d8aa84b636517685d
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-20 19:38:07 -0500

    class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends Component {
+  render() {
+    return <div></div>;
+  }
 }
-
-export default App;
